8e72659b3a37307d3286a7494cab8f185341c46e
gaopeng <592908853@qq.com>
2021-11-27 14:55:25 +0800

    feat: 路由权限控制

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import installElementPlus from './plugins/element'
 import '@/styles/index.scss' // 导入全局样式
 import installIcons from '@/icons'
+// 导入权限控制模块
+import './permission'
 
 const app = createApp(App)
 installElementPlus(app)
